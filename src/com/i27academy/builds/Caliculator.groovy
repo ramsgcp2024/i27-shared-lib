72a18c2ba80d1199305a4f9f5000a1b971ee4836
ramsgcp2024 <ramsgcp2024@gmail.com>
2024-07-03 23:07:11 +0530

    added methods

---
+++ src/com/i27academy/builds/Caliculator.groovy
@@ -0,0 +1,8 @@
+package com.i27academy.builds
+
+class Calculator {
+    def jenkins
+    Calculator(jenkins) {
+        this.jenkins = jenkins
+    }
+}
---
+++ src/com/i27academy/builds/Docker.groovy
@@ -0,0 +1,15 @@
+package com.i27academy.builds
+
+class Docker {
+    def jenkins 
+    Docker(jenkis) {
+        this.jenkis = jenkins
+    }
+    
+}
+
+// Addition method
+
+def add(firstNumber, secondNumber) {
+    return firstNumber + secondNumber
+}
