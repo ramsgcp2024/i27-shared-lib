0329f6753607a9cc9cf0e845fdad4a703ff8e66a
ramsgcp2024 <ramsgcp2024@gmail.com>
2024-07-09 23:27:33 +0530

    added k8s login code

---
+++ src/com/i27academy/k8s/K8s.groovy
@@ -0,0 +1,18 @@
+package com.i27academy.k8s
+
+class K8s {
+    def jenkins 
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    // Method to authoticate to cluster
+    def auth_login() {
+        jenkins.sh """
+        echo "Connecting to Kubernates Cluster through ****JENKINS****" 
+        # gcloud auth activate-service-account jenkins@instant-droplet-410306.iam.gserviceaccount.com --key-file=${}
+        gcloud compute instances list
+        """
+    }
+
+}
